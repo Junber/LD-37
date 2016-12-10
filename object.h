e8d930024a32f76f83340db602c8717d3864d181
Junber <derjulian.berger@web.de>
2016-12-10 22:21:24 +0100

    Made name of player image lowercase

--- object.h
+++ object.h
@@ -42,7 +42,7 @@ class Player: public Object
 public:
     int lastpos[2];
 
-    Player() : Object(20,20,"Player",4,"") {}
+    Player() : Object(20,20,"player",4,"") {}
     void update();
 };
 extern Player* player;
