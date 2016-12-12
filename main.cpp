ddb3bc2ac304e2eff5ad57ea1044b952fece42cf
Junber <derjulian.berger@web.de>
2016-12-12 23:02:43 +0100

    Day 2 done

--- main.cpp
+++ main.cpp
@@ -64,7 +64,8 @@ bool comp(Object* a, Object* b)
 
 void options()
 {
-    int cursor_pos=0, music_volume=Mix_VolumeMusic(100);
+    Mix_VolumeMusic(100);
+    int cursor_pos=0, music_volume=100;
     bool fullscreen=false;
     SDL_Event e;
     bool breakkk = false;
@@ -87,11 +88,12 @@ void options()
                     case SDLK_w:
                         cursor_pos--;
                         break;
-                    case SDLK_a:
+                    case SDLK_d:
                         if (cursor_pos == 0) sfx_volume+=10;
                         if (cursor_pos == 1)
                         {
                             music_volume+=10;
+                            if (music_volume>100)music_volume=100;
                             Mix_VolumeMusic(music_volume);
                         }
                         if (cursor_pos == 2)
@@ -100,11 +102,12 @@ void options()
                             SDL_SetWindowFullscreen(renderwindow,fullscreen*SDL_WINDOW_FULLSCREEN);
                         }
                         break;
-                    case SDLK_d:
+                    case SDLK_a:
                         if (cursor_pos == 0) sfx_volume-=10;
                         if (cursor_pos == 1)
                         {
                             music_volume-=10;
+                            if (music_volume<0)music_volume=0;
                             Mix_VolumeMusic(music_volume);
                         }
                         if (cursor_pos == 2)
@@ -120,11 +123,16 @@ void options()
         cursor_pos %= 3;
         while (cursor_pos < 0) cursor_pos += 3;
 
-        render_text(125,50,std::to_string(sfx_volume)+"%",255);
+        if (sfx_volume>100)sfx_volume=100;
+        if (sfx_volume<0)sfx_volume=0;
 
         SDL_RenderCopy(renderer,load_image("menu3options"+std::to_string(cursor_pos+1)),nullptr, nullptr);
-        SDL_RenderPresent(renderer);
 
+        render_text(130,87,std::to_string(sfx_volume)+" %",0);
+        render_text(130,102,std::to_string(music_volume)+" %",0);
+        render_text(130,117,fullscreen?"On":"Off",0);
+
+        SDL_RenderPresent(renderer);
         limit_fps();
     }
 }
@@ -190,6 +198,7 @@ int main(int argc, char* args[])
     SDL_SetTextureBlendMode(overlay,SDL_BLENDMODE_MOD);
 
     main_menu();
+    if (breakk) return 0;
 
     player = new Player();
 
@@ -197,7 +206,7 @@ int main(int argc, char* args[])
     int bg_size[2];
     SDL_QueryTexture(bg,nullptr,nullptr,&bg_size[0],&bg_size[1]);
 
-    load_script("start",&player->script);
+    load_script("end_day1",&player->script);//("start",&player->script);
     execute_script(player->script);
     player->script.clear();
 
